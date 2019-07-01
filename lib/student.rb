794a158e7b3d32f911981eb31f0b845d4050aece
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-01 23:30:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade
 attr_reader :id
 @@all = []
 
@@ -11,6 +12,9 @@ def initialize(name, grade, id=nil)
   @id = id
 @@all < self
 end
+
+def self.create_table
+  sql = <<-
 
 
 end
