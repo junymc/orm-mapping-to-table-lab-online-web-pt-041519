eff360ffd6d8c89c8d7180d5d9ea4b8674692884
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-01 23:34:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,15 @@ class Student
   end
 
   def self.create_table
-    sql = <<-
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute
+  end
 
 
 end
